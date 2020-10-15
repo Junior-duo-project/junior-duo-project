730ae394bb60c87d2a46ff49299503606c6da0c0
alasoui <alaasoui29@gmail.com>
2020-10-15 16:27:40 +0100

    get started

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(express.static(__dirname+'/public'));
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+
+
+
+
+let port= 3000;
+
+app.listen(port,()=>{console.log(`listening on port http://localhost:${port}`)})
+
